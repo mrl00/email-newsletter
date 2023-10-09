af0ff97a58fc8106ba32e22327e12440badf963b
mrl00 <m2l_0@outlook.com>
2023-10-09 18:20:14 -0300

    refactor: change field names

--- migrations/20231008141029_create_subscriptions_table.sql
+++ migrations/20231008141029_create_subscriptions_table.sql
@@ -1,8 +1,8 @@
 -- Add migration script here
 CREATE TABLE
   subscriptions (
-    sk_subscription uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
-    email TEXT NOT NULL UNIQUE,
-    name TEXT NOT NULL,
-    subscribed_at timestamptz NOT NULL
+    sk_subscription UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
+    tx_email TEXT NOT NULL UNIQUE,
+    tx_name TEXT NOT NULL,
+    dh_subscribed_at timestamptz NOT NULL
   );

54004bb119c9b8174560d94bddc5f4299d26ba94
mrl00 <m2l_0@outlook.com>
2023-10-09 18:20:36 -0300

    refactor: refactor code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 use std::net::TcpListener;
 
-use email_newsletter::run;
+use email_newsletter::{configuration::get_configuration, startup};
+use sqlx::{Connection, PgConnection};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
-    run(listener)?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to postgres");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener = TcpListener::bind(address)?;
+
+    startup::run(listener, connection)?.await
 }
--- tests/heath_check.rs
+++ tests/heath_check.rs
@@ -1,11 +1,22 @@
 use std::net::TcpListener;
 
+use email_newsletter::{
+    configuration::{self, get_configuration},
+    startup,
+};
+use sqlx::{Connection, PgConnection};
+
 fn spawn_app() -> String {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .expect("Failed to connect to postgres");
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
 
     let port = listener.local_addr().unwrap().port();
 
-    let server = email_newsletter::run(listener).expect("Failed to bind address");
+    let server = startup::run(listener, connection).expect("Failed to bind address");
 
     let _ = tokio::spawn(server);
 
@@ -34,6 +45,13 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
+    let config = configuration::get_configuration().expect("Failed to read configuration");
+    let connection_string = config.database.connection_string();
+
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -46,6 +64,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request.");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT tx_email, tx_name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.tx_email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.tx_name, "le guin");
 }
 
 #[tokio::test]

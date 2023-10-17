b47ce43389ed3a84ad717d2ebff768a57efc0022
mrl00 <m2l_0@outlook.com>
2023-10-17 19:36:46 -0300

    feat: configuring log
    We are falling back to printing all logs at info-level or above if the RUST_LOG environment variable has not been set.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::net::TcpListener;
 
 use email_newsletter::{configuration::get_configuration, startup};
+use env_logger::Env;
 use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("Failed to read configuration.");
 
     let connection_pool = PgPool::connect(&configuration.database.connection_string())

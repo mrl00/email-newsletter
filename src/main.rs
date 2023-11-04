f229f17e2ad59a2c64ae392ae504bcfa844b8ea0
mrl00 <m2l_0@outlook.com>
2023-11-04 18:07:54 -0300

    feat: use db config as secret

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,20 @@ use email_newsletter::{
     startup,
     telemetry::{get_subscriber, init_subscriber},
 };
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let subscriber = get_subscriber("email_newsletter".into(), "info".into());
-
+    let subscriber = get_subscriber("email_newsletter".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
 
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to postgres");
+    let connection_pool =
+        PgPool::connect(&configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to postgres");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
 

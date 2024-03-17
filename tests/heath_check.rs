5be881e9553094f81afb440b31ccaca5ba11268c
mrl00 <m2l_0@outlook.com>
2024-03-17 20:47:20 -0300

    test: change name valitor test

--- tests/heath_check.rs
+++ tests/heath_check.rs
@@ -146,11 +146,11 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
         ("", "missing both"),
     ];
 
-    for (invalid_doby, error_message) in test_cases {
+    for (invalid_body, error_message) in test_cases {
         let response = client
             .post(&format!("{}/subscriptions", &app.address))
             .header("Content-Type", "application/x-www-form-urlencoded")
-            .body(invalid_doby)
+            .body(invalid_body)
             .send()
             .await
             .expect("Failed to execute request.");
@@ -165,7 +165,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 }
 
 #[tokio::test]
-async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
+async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
     let app = spawn_app().await;
     let client = reqwest::Client::new();
     let test_cases = vec![
@@ -184,9 +184,9 @@ async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
             .expect("Failed to execute request.");
 
         assert_eq!(
-            200,
+            400,
             response.status().as_u16(),
-            "The API did not return a 200 OK when the payload was {}.",
+            "The API did not return a 400 Bad Request when the payload was {}.",
             description
         )
     }

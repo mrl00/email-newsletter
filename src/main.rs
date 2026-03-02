e734484b5b7806a9ca4cd1c5862151261e392970
mrl00 <m2l_0@outlook.com>
2026-03-02 13:37:39 -0300

    refactor: format

--- src/main.rs
+++ src/main.rs
@@ -15,17 +15,16 @@ async fn main() -> Result<(), std::io::Error> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
 
-    let connection_pool =
-        PgPoolOptions::new()
-            .acquire_timeout(std::time::Duration::from_secs(5))
-            .connect_lazy(&configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect to postgres");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(5))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
+        .expect("Failed to connect to postgres");
 
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
     );
-    
+
     let listener = TcpListener::bind(address)?;
 
     startup::run(listener, connection_pool)?.await
